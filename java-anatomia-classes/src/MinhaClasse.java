56e3a38cc8385974ba1bf60a5ca4d4a8c6bf2f01
Rodolfo Santana <rdmoedas@gmail.com>
2023-04-16 13:55:50 -0300

    update with leasson 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,8 +1,18 @@
 public class MinhaClasse {
     
     public static void main (String [] args) {
-        System.out.print ( "Olá mundo!" );
+        System.out.println( "Olá mundo!" );
+
+        String primeiroNome = "Rodolfo";
+        String segundoNome = "Santana";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.print(nomeCompleto);
     }
 
 
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
