5131d264ef2c3a8fef04e8e2fc736c21cda52319
Rodolfo Santana <rdmoedas@gmail.com>
2023-04-20 01:28:12 -0300

    feat: add leasson two

---
+++ pilha/src/one/digitalinnovation/Pilha.java
@@ -0,0 +1,40 @@
+package one.digitalinnovation;
+
+public class Pilha {
+
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    public void Push(No novoNo) {
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar);
+    }
+
+    public No Pop() {
+        if(!this.isEmpty()) {
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+            return noPoped;
+        }
+        return null;
+    }
+
+    public No top() {
+        return refNoEntradaPilha;
+    }
+
+    public boolean isEmpty() {
+
+        // if (refNoEntradaPilha == null) {
+        //     return true;
+        // }
+        // return false;
+
+        return refNoEntradaPilha == null ? true : false;
+    }
+
+}
